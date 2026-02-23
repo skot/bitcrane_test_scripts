06b862dbea472ed6b4aad39a5d1285b044433228
Skot <skot@users.noreply.github.com>
2026-02-23 17:23:39 -0500

    update oled_test.py to take command line arguments

--- oled_test.py
+++ oled_test.py
@@ -4,26 +4,15 @@ import sys
 
 import bitcrane
 
-# # Take command line arguments for FAN1 and FAN2 speeds
-# if len(sys.argv) != 3:
-#     print("Usage: python fan_tester.py <fan1_speed> <fan2_speed>")
-#     print("  fan1_speed: 0-100")
-#     print("  fan2_speed: 0-100")
-#     exit(1)
+# Take command line argument for display string
+if len(sys.argv) != 2:
+    print("Usage: python oled_test.py <string>")
+    print("  string: Text to display (max 32 chars)")
+    print("  Note: If string contains a space, it will be split - value before last space")
+    print("        appears in large font on top, text after appears in small font on bottom")
+    exit(1)
 
-# try:
-#     fan1_speed = int(sys.argv[1])
-#     fan2_speed = int(sys.argv[2])
-    
-#     if fan1_speed < 0 or fan1_speed > 100:
-#         print("Error: FAN1 speed must be between 0 and 100")
-#         exit(1)
-#     if fan2_speed < 0 or fan2_speed > 100:
-#         print("Error: FAN2 speed must be between 0 and 100")
-#         exit(1)
-# except ValueError:
-#     print("Error: Both fan speeds must be numbers between 0 and 100")
-#     exit(1)
+display_string = sys.argv[1]
 
 # Configure the serial ports
 try:
@@ -40,5 +29,8 @@ def prettyHex(data):
     return ' '.join(f'{byte:02X}' for byte in data)
 
 
-# print hello on the display
-bitcrane.display(serial_port_ctrl, "33.666 TH/s", debug=True)
+# Display the string on the OLED
+bitcrane.display(serial_port_ctrl, display_string, debug=True)
+
+# Close the serial port
+serial_port_ctrl.close()
